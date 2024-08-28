56e717e81571f2dfad9beda6017cc0e2fa0fbe16
Nasiba18 <pirimbaevna1@gmail.com>
2024-08-28 15:45:08 -0500

    added modul

---
+++ terraform/variable.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  type        = string
+  default     = "wordpress"
+  description = "my wordpress application"
+}
+
+variable "chart" {
+  type        = string
+  default     = "../myapp"
+  description = "location of my wordpress app chart"
+}
+
+variable "namespace" {
+  type        = string
+  default     = "mywordpressapp"
+  description = "the namespace for app"
+}
+
+variable value {
+  type = list
+  default = []
+  description = "values for my chart"
+}
